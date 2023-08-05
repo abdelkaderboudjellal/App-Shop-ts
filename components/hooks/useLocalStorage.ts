92059853c5a04116edb4ad0388ba56a7778e75ce
Abdelkader boudjellal <99020132+abdelkaderboudjellal@users.noreply.github.com>
2023-08-05 23:56:07 +0100

    update localStorage

--- components/hooks/useLocalStorage.ts
+++ components/hooks/useLocalStorage.ts
@@ -1,20 +1,29 @@
 import { useEffect, useState } from "react";
 
-export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
-  const [value, setValue] = useState<T>(() => {
-    const jsonValue = localStorage.getItem(key);
+export function useLocalStorage<T>(key: string, initialValue: T) {
+  /*   const getFromLocalStorage = (key: string) => {
+    if (!key || typeof window === 'undefined') {
+        return ""
+    }
+    return localStorage.getItem(key)
+  } */
+  const [value, setValue] = useState(initialValue);
+  useEffect(() => {
+    /*   const jsonValue =localStorage.getItem(key)
     if (jsonValue != null) return JSON.parse(jsonValue);
 
     if (typeof initialValue === "function") {
       return (initialValue as () => T)();
     } else {
       return initialValue;
-    }
-  });
+    } */
+    const stored = localStorage.getItem(key);
+    setValue(stored ? JSON.parse(stored) : initialValue);
+  }, []);
 
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
   }, [key, value]);
 
-  return [value, setValue] as [typeof value, typeof setValue];
+  return [value, setValue] as const /* [typeof value, typeof setValue] */;
 }
